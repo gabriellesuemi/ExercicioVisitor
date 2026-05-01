3c72685f02eb8fbddc62fc6ac281af5433128a3c
gabriellesuemi <gabrielle.alencar@unifesp.br>
2026-05-01 19:45:10 -0300

    adiciona classe Main para execução do sistema

--- Main.java
+++ Main.java
@@ -1,3 +1,18 @@
 public class Main {
-    
+    public static void main(String[] args) {
+
+        Relatorio r1 = new RelatorioA();
+        Relatorio r2 = new RelatorioB();
+
+        Visitor pdf = new PDFVisitor();
+        Visitor xml = new XMLVisitor();
+        Visitor html = new HTMLVisitor();
+        Visitor planilha = new PlanilhaVisitor();
+
+        r1.aceitar(pdf);
+        r1.aceitar(html);
+
+        r2.aceitar(xml);
+        r2.aceitar(planilha);
+    }
 }
